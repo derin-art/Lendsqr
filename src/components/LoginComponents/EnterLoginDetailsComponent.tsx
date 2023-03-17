3a33f3640ed9d7429a4b78e2f30e72af7711f26b
Derin Owoade <owoadederin6@gmail.com>
2023-03-17 22:21:34 +0100

    Added input elements for EnterLoginDetailsComponent

--- src/components/LoginComponents/EnterLoginDetailsComponent.tsx
+++ src/components/LoginComponents/EnterLoginDetailsComponent.tsx
@@ -1,7 +1,14 @@
 export default function EnterLoginDetailsComponent() {
   return (
     <div className="LoginDetailComp">
-      <div>Welcome</div>
+      <h1>Welcome</h1>
+      <h2>Enter Login Details.</h2>
+      <div className="w-full">
+        <input></input>
+      </div>
+      <div className="w-full">
+        <input></input>
+      </div>
     </div>
   );
 }
