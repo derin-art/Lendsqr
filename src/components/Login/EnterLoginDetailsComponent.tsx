75c87eab01e33cbbc0912b64c5869b27a0d7cefa
Derin Owoade <owoadederin6@gmail.com>
2023-03-18 20:16:50 +0100

    Applied Avenir font family to Login Component

--- src/components/Login/EnterLoginDetailsComponent.tsx
+++ src/components/Login/EnterLoginDetailsComponent.tsx
@@ -4,21 +4,21 @@ export default function EnterLoginDetailsComponent() {
   return (
     <div className={`${Style.LoginDetailComp}`}>
       <div className={`${Style.headingCont}`}>
-        <h1>Welcome!</h1>
-        <h2>Enter details to login.</h2>
+        <h1 className={Style.h1}>Welcome!</h1>
+        <h2 className={Style.h2}>Enter details to login.</h2>
       </div>
       <div className={`w-full ${Style.inputCont}`}>
         <input placeholder="Email"></input>
       </div>
       <div className={`w-full ${Style.inputCont}`}>
         <input placeholder="Password"></input>
-        <button className={Style.ShowInput}>Show</button>
+        <button className={Style.ShowInput}>SHOW</button>
       </div>
       <div className={Style.ForgotBtn}>
         <button>FORGOT PASSWORD?</button>
       </div>
       <div className={`${Style.LoginBtnCont}`}>
-        <button>Login</button>
+        <button>LOG IN</button>
       </div>
     </div>
   );
