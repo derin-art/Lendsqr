1c626722ccf25414c0410e9453656e894bac9825
Derin Owoade <owoadederin6@gmail.com>
2023-03-18 22:15:42 +0100

    Added styling for Header component Laptop view/Screen

--- src/components/Dashboard/Header.tsx
+++ src/components/Dashboard/Header.tsx
@@ -2,23 +2,42 @@ import Style from "./Dashboard.module.scss";
 
 export default function Header() {
   return (
-    <div className={`${Style.Header}`}>
-      <img className={`${Style.Logo}`} src="/Icons/Logo.svg"></img>
-      <div className={Style.InfoHeader}>
-        <span>Menu</span>{" "}
-        <span className={Style.ProfilePicCont}>
-          {" "}
-          <span>Docs</span>
-          <img className={Style.BellIcon} src="/Icons/Header/Bell.svg"></img>
-          <img
-            src="/Images/Header/ProfilePic.png"
-            className={Style.ProfilePic}
-          ></img>
-          <span>Adedeji</span>
-        </span>
-      </div>
+    <div>
+      <div className={`${Style.Header}`}>
+        <img className={`${Style.Logo}`} src="/Icons/Logo.svg"></img>
+        <div className={Style.InfoHeader}>
+          <span>Menu</span>{" "}
+          <span className={Style.ProfilePicCont}>
+            {" "}
+            <span>Docs</span>
+            <img className={Style.BellIcon} src="/Icons/Header/Bell.svg"></img>
+            <img
+              src="/Images/Header/ProfilePic.png"
+              className={Style.ProfilePic}
+            ></img>
+            <span>Adedeji</span>
+          </span>
+        </div>
 
-      <input className=""></input>
+        <input className=""></input>
+      </div>
+      <div className={`${Style.HeaderTab}`}>
+        <img className={`${Style.Logo}`} src="/Icons/Logo.svg"></img>
+
+        <input className=""></input>
+        <div className={Style.InfoHeader}>
+          <span className={Style.ProfilePicCont}>
+            {" "}
+            <span>Docs</span>
+            <img className={Style.BellIcon} src="/Icons/Header/Bell.svg"></img>
+            <img
+              src="/Images/Header/ProfilePic.png"
+              className={Style.ProfilePic}
+            ></img>
+            <span>Adedeji</span>
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
