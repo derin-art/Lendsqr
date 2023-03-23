dddf438c9ee31bd0a4e4295034f1d5e82f36928d
Derin Owoade <owoadederin6@gmail.com>
2023-03-23 14:32:44 +0100

    Arranged the Contents of the UserDetail page into an object for rendering

--- src/components/UserDetailsPage/UserDetailsPage.tsx
+++ src/components/UserDetailsPage/UserDetailsPage.tsx
@@ -10,7 +10,78 @@ export default function UserDetailPage() {
 
   useEffect(() => {}, []);
   const userDetailPageInfo = [
-    { name: "Personal Information", subHeaders: [[], []] },
+    {
+      name: "Personal Information",
+      subHeaders: [
+        [
+          {
+            name: "FULL NAME",
+            value: `${userDetails?.profile.firstName} ${userDetails?.profile.lastName}`,
+          },
+          { name: "email Address", value: userDetails?.email },
+          { name: "PHONE NUMBER", value: userDetails?.phoneNumber },
+          { name: "BVN", value: userDetails?.profile.gender },
+        ],
+        [
+          { name: "Marital Status", value: "Single" },
+          { name: "Children", value: "None" },
+          { name: "Type of Residence", value: "Parent's Apartment" },
+        ],
+      ],
+    },
+
+    {
+      name: "Education and Employement",
+      subHeaders: [
+        [
+          { name: "Level of Education", value: userDetails?.education.level },
+          {
+            name: "Employement Status",
+            value: userDetails?.education.employmentStatus,
+          },
+          {
+            name: "Sector of Employement",
+            value: userDetails?.education.sector,
+          },
+          {
+            name: "Duration of Employement",
+            value: userDetails?.education.duration,
+          },
+        ],
+        [
+          { name: "Office Mail", value: userDetails?.education.officeEmail },
+          {
+            name: "Monthly income",
+            value: userDetails?.education.monthlyIncome,
+          },
+          {
+            name: "Loan Repayment",
+            value: userDetails?.education.loanRepayment,
+          },
+        ],
+      ],
+    },
+    {
+      name: "Socials",
+      subHeaders: [
+        { name: "Twitter", value: userDetails?.socials.twitter },
+        { name: "Facebook", value: userDetails?.socials.facebook },
+        { name: "Instagram", value: userDetails?.socials.instagram },
+      ],
+    },
+    {
+      name: "Guarantor",
+      subHeaders: [
+        {
+          name: "Full Name",
+          value: `${userDetails?.guarantor.firstName} 
+           ${userDetails?.guarantor.lastName}`,
+        },
+        { name: "Phone Number", value: userDetails?.guarantor.phoneNumber },
+        { name: "Email", value: userDetails?.guarantor.address },
+        { name: "RelationShip", value: "Sister" },
+      ],
+    },
   ];
 
   return <div className={Style.UserDetailsComp}>{id}</div>;
