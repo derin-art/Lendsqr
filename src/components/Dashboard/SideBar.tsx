223d6336bec9fc82ac5cf05cfad173d756a62346
Derin Owoade <owoadederin6@gmail.com>
2023-03-25 17:56:00 +0100

    Added styling to Sidebar SwitchOrg button, Added Borders to UsersPageTableItem

--- src/components/Dashboard/SideBar.tsx
+++ src/components/Dashboard/SideBar.tsx
@@ -108,6 +108,9 @@ export default function SideBar(props: SideBarProps) {
       <div className={` ${Style.SwitchOrg} `}>
         <img src={`${sideBarSvgRootLocation}/SwitchOrg.svg`}></img>
         <span>Switch Organization</span>
+        <button>
+          <img src={`${sideBarSvgRootLocation}/SwitchOrgDownArrow.svg`}></img>
+        </button>
       </div>
       <div className={Style.Dashboard}>
         <img src={`${sideBarSvgRootLocation}/Dashboard.svg`}></img>
