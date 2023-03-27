442b38e04424be817ae7171c07d6254cb19c55e7
Derin Owoade <owoadederin6@gmail.com>
2023-03-27 23:20:58 +0100

    Added logic for UserPage toastify Popup to only render for screens above 640px width, as it is not relevant for mobile views

--- src/components/UsersPage/UsersPage.tsx
+++ src/components/UsersPage/UsersPage.tsx
@@ -9,14 +9,14 @@ export default function Users() {
   const toastId2 = "custom";
 
   useEffect(() => {
-    console.log("heyy");
-    toast.info(
-      "Hi User. Click on the table header to open up the filter component. Click to close",
-      {
-        autoClose: false,
-        toastId: toastId2,
-      }
-    );
+    window.innerWidth > 640 &&
+      toast.info(
+        "Hi User. Click on the table header to open up the filter component. Click to close",
+        {
+          autoClose: false,
+          toastId: toastId2,
+        }
+      );
   }, []);
   return (
     <div className={Style.UsersPage}>
