56a3e1f9d4eb6ac97b0ebf36e6e0c077b17c4a58
Derin Owoade <owoadederin6@gmail.com>
2023-03-27 23:13:38 +0100

    Added logic that points to click on the table's header to open the filter component

--- src/components/UsersPage/UsersPage.tsx
+++ src/components/UsersPage/UsersPage.tsx
@@ -1,10 +1,26 @@
 import UserPageHeadline from "./UserPageHeadline";
 import UsersPageTable from "./UsersPageTable";
 import Style from "./UsersPage.module.scss";
+import { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Users() {
+  const toastId2 = "custom";
+
+  useEffect(() => {
+    console.log("heyy");
+    toast.info(
+      "Hi User. Click on the table header to open up the filter component. Click to close",
+      {
+        autoClose: false,
+        toastId: toastId2,
+      }
+    );
+  }, []);
   return (
     <div className={Style.UsersPage}>
+      <ToastContainer></ToastContainer>
       <UserPageHeadline></UserPageHeadline>
       <UsersPageTable></UsersPageTable>
     </div>
